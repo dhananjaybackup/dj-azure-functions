061fef1562dfe13f8e7642a2a3881a3f51659546
dhananjaybackup <dhananjaybackup7@gmail.com>
2025-12-21 12:37:30 +0530

    update message

--- DJFunctions/Functions/GetValtSecretMessage.cs
+++ DJFunctions/Functions/GetValtSecretMessage.cs
@@ -21,7 +21,7 @@ public class GetValtSecretMessage
         var secret = Environment.GetEnvironmentVariable("AppMessage");
 
         var response = req.CreateResponse(HttpStatusCode.OK);
-        response.WriteString($"Message from Key Vault updated message: {secret}");
+        response.WriteString($"Message from Key Vault deployment: {secret}");
         return response;
     }
 }
