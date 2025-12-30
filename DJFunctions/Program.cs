90fb7a7c3452eea4fc3a7bbdb2596d1fee6138a8
dhananjaybackup <dhananjaybackup7@gmail.com>
2025-12-30 18:51:43 +0530

    added functionality to log tobe seen in functionapp app insite livematrics

--- DJFunctions/Program.cs
+++ DJFunctions/Program.cs
@@ -1,8 +1,8 @@
 
-
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
@@ -10,6 +10,11 @@ var host = new HostBuilder()
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+    })
+    .ConfigureLogging(logging =>
+    {
+        logging.AddApplicationInsights();
+        logging.SetMinimumLevel(LogLevel.Information);
     })
     .Build();
 
