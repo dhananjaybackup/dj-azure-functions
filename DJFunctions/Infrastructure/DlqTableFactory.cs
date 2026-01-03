1468cdd0a0f5552f48ca5421653748bcd61735a2
dhananjaybackup <dhananjaybackup7@gmail.com>
2026-01-03 20:48:45 +0530

    changed AzureWebJobsStorage to AccountName was getting erro

--- DJFunctions/Infrastructure/DlqTableFactory.cs
+++ DJFunctions/Infrastructure/DlqTableFactory.cs
@@ -1,15 +1,16 @@
 using Azure.Data.Tables;
+using Azure.Identity;
 
 public static class DlqTableFactory
 {
     public static TableClient GetDlqTable()
     {
-        var conn = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
-        var client = new TableServiceClient(conn);
+        var accountName = Environment.GetEnvironmentVariable("StorageAccountName");
+        var service = new TableServiceClient(
+            new Uri($"https://{accountName}.table.core.windows.net"),
+            new DefaultAzureCredential());
 
-        var table = client.GetTableClient("WorkflowDLQ");
-        table.CreateIfNotExists();
-
+        var table = service.GetTableClient("WorkflowDLQ");
         return table;
     }
 }
